c7f6d29decf824babfa6ec4cd52966b94cc3d385
Luka Markac <luka.markac@ericsson.com>
2020-09-18 12:43:32 +0200

    Initial commit

---
+++ src/main/java/com/luka/internal/springDemo/dao/PersonDataAccessService.java
@@ -0,0 +1,83 @@
+package com.luka.internal.springDemo.dao;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.luka.internal.springDemo.model.Person;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao{
+	
+	private final JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+		super();
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		return 0;
+	}
+
+	@Override
+	public List<Person> selectAllPeople() {
+		
+		final String sql = "SELECT id, name FROM person;";
+		
+		return jdbcTemplate.query(sql, (resultSet, i) ->{
+			
+			UUID id = UUID.fromString(resultSet.getString("id"));
+			String name = resultSet.getString("name");
+			
+			return new Person(id, name);			
+		});
+	}
+
+	@Override
+	public Optional<Person> selectPersonById(UUID id) {
+		
+		final String sql = "SELECT id, name FROM person WHERE id = ?";
+		
+		Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) ->{
+			
+			UUID uuid = UUID.fromString(resultSet.getString("id"));
+			String name = resultSet.getString("name");
+			return new Person(uuid, name);
+		});
+		
+		return Optional.ofNullable(person);
+	}
+
+	@Override
+	public int deletePersonById(UUID id) {
+
+		final String sql = "DELETE FROM person WHERE id = ?";
+		
+		Object[] args = new Object[] {id};		
+		
+		return jdbcTemplate.update(sql, args);
+		
+	}
+
+	@Override
+	public int updatePersonById(UUID id, Person person) {
+		
+		System.out.println("Ulazim u metodu.");
+		
+		final String sql = "UPDATE person SET id = ? WHERE id = ?";
+		
+		Object[] args = new Object[] {id, person.getId()};
+		
+		return jdbcTemplate.update(sql, args);
+		
+	}
+
+}
---
+++ src/test/java/com/luka/internal/springDemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luka.internal.springDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
